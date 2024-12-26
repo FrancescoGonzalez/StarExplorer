48f6e465dd3e2a73a7fbdc1e6fff53edc45ca8c5
FrancescoGonzalez <francibgonza21@gmail.com>
2024-12-26 14:46:13 +0100

    optimized rest.dart

---
+++ lib/data/DSO_data.dart
@@ -0,0 +1,18 @@
+final dsoNames = [
+  "M31", // Andromeda Galaxy
+  "M42", // Orion Nebula
+  "M57", // Ring Nebula
+  "M51", // Whirlpool Galaxy
+  "M13", // Hercules Cluster
+  "M104", // Sombrero Galaxy
+  "M16", // Eagle Nebula
+  "Abell 426", // Perseus Cluster
+  "M81", // Bode's Galaxy
+  "M33", // Triangulum Galaxy
+  "Leo Triplet", // Leo Triplet (M65, M66, NGC 3628)
+  "M8", // Lagoon Nebula
+  "NGC 6210", // Great Hercules Cluster
+  "M104", // Sombrero Galaxy (repeated entry)
+  "LMC", // Large Magellanic Cloud
+  "SMC" // Small Magellanic Cloud
+];
--- lib/model/space_object_data_alt_az.dart
+++ lib/model/space_object_data_alt_az.dart
@@ -2,17 +2,20 @@ class SpaceObjectDataAltAz {
   final double alt;
   final double az;
   final String name;
+  final double? magnitude;
 
   SpaceObjectDataAltAz
 ({
     required this.alt,
     required this.az,
     required this.name,
+    this.magnitude,
 
   });
 
   double get getAlt => alt;
   double get getAz => az;
   String get getName => name;
+  double? get getMagnitude => magnitude;
 
 }
