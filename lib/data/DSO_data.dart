1bd12400c05025dbdadf5d08f9c06f02dec32051
FrancescoGonzalez <francibgonza21@gmail.com>
2025-03-07 14:07:59 +0100

    updated star and dso names on json

--- lib/data/DSO_data.dart
+++ lib/data/DSO_data.dart
@@ -1,83 +1,83 @@
 const dsoData = [
   {
-    "name": "M31",
+    "name": "MESSIER 031",
     "ra": 10.68458,
     "dec": 41.26917,
     "type": "galaxy",
     "mag": 3.44
   }, // Andromeda Galaxy
   {
-    "name": "M42",
+    "name": "MESSIER 042",
     "ra": 83.82292,
     "dec": -5.39111,
     "type": "nebula",
     "mag": 4.0
   }, // Orion Nebula
   {
-    "name": "M57",
+    "name": "MESSIER 057",
     "ra": 283.39625,
     "dec": 33.02917,
     "type": "nebula",
     "mag": 8.8
   }, // Ring Nebula
   {
-    "name": "M51",
+    "name": "MESSIER 051",
     "ra": 202.47042,
     "dec": 47.19528,
     "type": "galaxy",
     "mag": 8.4
   }, // Whirlpool Galaxy
   {
-    "name": "M13",
+    "name": "MESSIER 013",
     "ra": 250.42167,
     "dec": 36.46000,
     "type": "cluster",
     "mag": 5.8
   }, // Hercules Cluster
   {
-    "name": "M104",
+    "name": "MESSIER 104",
     "ra": 189.99750,
     "dec": -11.62306,
     "type": "galaxy",
     "mag": 8.0
   }, // Sombrero Galaxy
   {
-    "name": "M16",
+    "name": "MESSIER 016",
     "ra": 274.70000,
     "dec": -13.78333,
     "type": "nebula",
     "mag": 6.0
   }, // Eagle Nebula
   {
-    "name": "Abell 426",
+    "name": "Perseus Cluster",
     "ra": 49.94667,
     "dec": 41.51306,
     "type": "galaxy-cluster",
     "mag": 12.6
   }, // Perseus Cluster
   {
-    "name": "M81",
+    "name": "MESSIER 081",
     "ra": 148.88833,
     "dec": 69.06528,
     "type": "galaxy",
     "mag": 6.94
   }, // Bode's Galaxy
   {
-    "name": "M33",
+    "name": "MESSIER 033",
     "ra": 23.46208,
     "dec": 30.66000,
     "type": "galaxy",
     "mag": 5.72
   }, // Triangulum Galaxy
   {
-    "name": "Leo Triplet",
+    "name": "ARP 317",
     "ra": 169.73292,
     "dec": 12.99167,
     "type": "galaxy-cluster",
     "mag": 9.0
   }, // M65, M66, NGC 3628
   {
-    "name": "M8",
+    "name": "MESSIER 008",
     "ra": 270.90417,
     "dec": -24.38667,
     "type": "nebula",
@@ -91,14 +91,14 @@ const dsoData = [
     "mag": 8.8
   }, // Turtle Nebula
   {
-    "name": "LMC",
+    "name": "Large Magellanic Cloud",
     "ra": 80.89375,
     "dec": -69.75611,
     "type": "galaxy",
     "mag": 0.9
   }, // Large Magellanic Cloud
   {
-    "name": "SMC",
+    "name": "Small Magellanic Cloud",
     "ra": 13.15833,
     "dec": -72.80028,
     "type": "galaxy",
